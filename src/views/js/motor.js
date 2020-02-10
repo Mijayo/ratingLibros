12c8aff3cf01804d470cdf6675e0f0a38969ccbe
Diego <48282657+Mijayo@users.noreply.github.com>
2020-02-10 12:00:05 +0100

    test_V05

--- src/views/js/motor.js
+++ src/views/js/motor.js
@@ -1,3 +1,11 @@
 function updateTextInput(val) {
     document.getElementById('textInput').value = `${val} estrellas`;
 }
+
+const btn = document.getElementById("btnRegistrar");
+
+btn.addEventListener("click", p());
+
+function p() {
+    alert('hola');
+}
