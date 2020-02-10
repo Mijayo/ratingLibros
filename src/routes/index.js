363be07f4955c2d84a87aeef94a9e980584d91cb
Diego <48282657+Mijayo@users.noreply.github.com>
2020-02-10 16:33:09 +0100

    test_V06

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+
+// create application/json parser
+const jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+router.post('/', urlencodedParser, (req, res) => {
+    const titulo = req.body.titulo;
+    const cali = req.body.estrella;
+    console.log(titulo);
+    res.send({
+        titulo,
+        cali
+    });
+});
 
 router.get('/', (req, res) => {
     res.render('index');
--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,12 @@
-// requires - express
 const express = require('express');
-const path = require('path');
-const ejs = require('ejs');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
 const routes = require('../routes/index');
+
 
 // rutas
 app.use('/', routes);
@@ -14,6 +17,13 @@ app.set('view engine', 'ejs');
 
 // estilos
 app.use(express.static(path.join(__dirname, '../views')));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
+
+// bbdd
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
 
 // puerto
-app.listen(3000, console.log('escuchando en el 3000'));
+app.listen(process.env.PORT || 3000, console.log('escuchando en el 3000'));
